4467ec7ee5b2bcf273990ce7b413f6e6e614b575
EllaHanson <ellahanson.126@gmail.com>
2025-04-15 18:19:13 -0700

    find by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,8 +41,15 @@ const users = {
       (user) => user["name"] === name
     );
   };
-  
-  app.get("/users", (req, res) => {
+
+  const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
       let result = findUserByName(name);
@@ -52,10 +59,16 @@ const users = {
       res.send(users);
     }
   });
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+  
+  app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
