803571f0cfe1216db22f33155b367f60dfcce40c
EllaHanson <ellahanson.126@gmail.com>
2025-04-22 23:49:50 -0700

    update

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import {getUsers, findUserById, findUserByName, findUserByJob} from "./user-services.js";
 
 const app = express();
 const port = 8000;
@@ -38,19 +39,25 @@ const users = {
     ]
   };
 
+  /*
   const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
+  */
 
+  /*
   const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+  */
 
+  /*
   const addUser = (user) => {
     users["users_list"].push(user);
     return user;
   };
+  */
 
   const randNum = () => {
     return Math.round(Math.random() * 10000).toString();
@@ -61,15 +68,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
-  });
+  const name = req.query.name;
+  const job = req.query.job;
+  let result = getUsers(name, job);
+  res.send(result);
+});
 
 app.post("/users", (req, res) => {
     const tempId = randNum();
@@ -84,7 +87,8 @@ app.post("/users", (req, res) => {
   
   app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
-    let result = findUserById(id);
+    let result = user-services.findUserById(id);
+    res.send(result);
     if (result === undefined) {
       res.status(404).send("Resource not found.");
     } else {
@@ -109,4 +113,3 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
-
